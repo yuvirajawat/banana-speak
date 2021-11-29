980ddb658c4f4da7ba31101b8c11207d2fc8c47e
YUVRAJ SINGH RAJAWAT <yuvirajawat193@gmail.com>
2021-11-29 20:49:33 +0530

    feat: api integration

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var userText = document.querySelector("#user-text");
 var bananaText = document.querySelector("#banana-text");
-
+const serverURL="https://api.funtranslations.com/translate/minion.json";
+function getTranslationURL(inputText){
+    return serverURL+"?"+"text="+inputText;
+}
+function errorHandler(error){
+    console.log("error occured!", error);
+    alert("something wrong on the server side! please try again after some time.");
+}
 function clickHandler(){
     // will show translated text
-    bananaText.innerText=" translated text: " + userText.value;
+    fetch(getTranslationURL(userText.value)).then(response=>response.json())
+    .then(json => {
+        bananaText.innerText=json.contents.translated;
+    })
+    .catch(errorHandler);
 }
 btnTranslate.addEventListener("click",clickHandler);
